9cf852c3cc5957172bab9e2178546dc0802c18ed
Mikalai88 <mikalai.mikhalachkin@gmail.com>
2023-11-22 20:07:00 +0100

    testing-route.ts changed 2

--- src/routes/testing-route.ts
+++ src/routes/testing-route.ts
@@ -6,7 +6,6 @@ export const testingRoute = Router({})
 
 testingRoute.delete('/all-data', (req: Request, res: Response) => {
     db.blogs.length = 0
-    db.videos.length = 0
     db.posts.length = 0
     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 })
