e682e0741b8116806e91b1a33bbae30d44678d05
Mikalai88 <mikalai.mikhalachkin@gmail.com>
2023-11-22 20:40:35 +0100

    auth-middleware.ts changed 2

--- src/middlewares/auth/auth-middleware.ts
+++ src/middlewares/auth/auth-middleware.ts
@@ -5,37 +5,37 @@ const login = "admin"
 const password = "qwerty"
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    // if (req.headers['authorization'] !== "Basic YWRtaW46cXdlcnR5") {
+    if (req.headers['authorization'] !== "Basic YWRtaW46cXdlcnR5") {
+        res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
+        return
+    }
+
+    return next()
+
+    // OR
+
+    // const auth = req.headers['authorization']
+    //
+    // if (!auth) {
+    //     res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
+    //     return
+    // }
+    //
+    // const [basic, token] = auth.split(" ")
+    //
+    // if (basic !== "Basic") {
+    //     res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
+    //     return
+    // }
+    //
+    // const decodedData = Buffer.from(token, 'base64').toString()
+    //
+    // const [decodedLogin, decodedPassword] = decodedData.split(":")
+    //
+    // if (decodedLogin !== login || decodedPassword !== password) {
     //     res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
     //     return
     // }
     //
     // return next()
-
-    // OR
-
-    const auth = req.headers['authorization']
-
-    if (!auth) {
-        res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
-        return
-    }
-
-    const [basic, token] = auth.split(" ")
-
-    if (basic !== "Basic") {
-        res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
-        return
-    }
-
-    const decodedData = Buffer.from(token, 'base64').toString()
-
-    const [decodedLogin, decodedPassword] = decodedData.split(":")
-
-    if (decodedLogin !== login || decodedPassword !== password) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-        return
-    }
-
-    return next()
 }
