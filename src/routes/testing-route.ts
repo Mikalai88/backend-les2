35299bf10e636af2be2873018fcdc7257ee0e4cb
Mikalai88 <mikalai.mikhalachkin@gmail.com>
2023-12-17 18:44:45 +0100

    dz5 fix

--- src/routes/testing-route.ts
+++ src/routes/testing-route.ts
@@ -1,12 +1,12 @@
 import {Router, Response, Request} from "express";
-import {blogCollection, postCollection, videoCollection} from "../db/db";
+import {blogCollection, postCollection, userCollection, videoCollection} from "../db/db";
 
 export const testingRoute = Router({})
 
 testingRoute.delete('/all-data', async (req: Request, res: Response) => {
     await blogCollection.deleteMany({})
     await postCollection.deleteMany({})
-    await videoCollection.deleteMany({})
+    await userCollection.deleteMany({})
 
     res.sendStatus(204)
 })
