f1338ee25eb069da7802bdbba8927f842b5b8bd1
Mikalai88 <mikalai.mikhalachkin@gmail.com>
2023-12-31 12:28:24 +0100

    dz7

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -16,7 +16,7 @@ export const emailAdapter = {
             subject: "Confirmation email.",
             html: ` <h1>Thank for your registration</h1>
                     <p>To finish registration please follow the link below:
-                    <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a>
+                    <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a> 
                     </p>`
         })
         return mailOptions.accepted.length > 0
